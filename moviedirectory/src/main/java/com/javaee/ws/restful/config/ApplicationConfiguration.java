0b88f8818a3f38c8a1221517acf0dbca5d88cfbe
JohnyzHub <jbshaik82@gmail.com>
2018-07-05 23:19:09 -0400

    JAX-RS: Restful Webservices: Service Configuration
    	Registering the rest service with the JAX-RS library

---
+++ moviedirectory/src/main/java/com/javaee/ws/restful/config/ApplicationConfiguration.java
@@ -0,0 +1,31 @@
+package com.javaee.ws.restful.config;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+import com.javaee.ws.restful.service.MovieDirectoryRestService;
+import com.javaee.ws.restful.service.exception.ArithmenticExceptionMapper;
+import com.javaee.ws.restful.service.sse.EventsResource;
+
+/**
+ * @author shaikjb
+ *
+ */
+@ApplicationPath("rest")
+public class ApplicationConfiguration extends Application {
+
+	Set<Class<?>> classes = new HashSet<Class<?>>(2);
+
+	@Override
+	public Set<Class<?>> getClasses() {
+		classes.add(MovieDirectoryRestService.class);
+		classes.add(ArithmenticExceptionMapper.class);
+		classes.add(EventsResource.class);
+		return classes;
+	}
+
+}
