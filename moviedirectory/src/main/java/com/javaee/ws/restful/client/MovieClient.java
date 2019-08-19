55326e112975452b23e1f4eea2a9dd9771c1acb6
JohnyzHub <jbshaik82@gmail.com>
2019-08-19 16:26:20 -0400

    JAX-RS: Client code for PUT
      Updated the client code to test the PUT method.

--- moviedirectory/src/main/java/com/javaee/ws/restful/client/MovieClient.java
+++ moviedirectory/src/main/java/com/javaee/ws/restful/client/MovieClient.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.javaee.ws.restful.service.entity.Movie;
 import com.javaee.ws.restful.service.entityprovider.CSVMessageBodyReaderWriter;
@@ -25,8 +27,10 @@ public class MovieClient {
 
 	public static void main(String[] args) throws Exception {
 		MovieClient client = new MovieClient();
-		client.findAllMovies();
+
 		client.findMovie();
+		client.updateMovie();
+		client.findAllMovies();
 
 	}
 
@@ -38,7 +42,15 @@ public class MovieClient {
 		});
 
 		String uriString = webTarget.getUri().toString();
-		System.out.println("\n"+uriString + ":: is being processed synchronously::\n" + movie);
+		System.out.println("\n" + uriString + ":: is being processed synchronously::\n" + movie);
+	}
+
+	public void updateMovie() throws Exception {
+		Client client = ClientBuilder.newBuilder().build();
+		WebTarget webTarget = client.target(BASE_URI).path("movie");
+		Movie movie = new Movie(1, "movie1", 10);
+		Response updatedMovieResponse = webTarget.request().put(Entity.entity(movie, MediaType.APPLICATION_JSON));
+		System.out.println("\nMovie entry is Updated: " + updatedMovieResponse.readEntity(String.class));
 	}
 
 	public void findAllMovies() throws Exception {
@@ -48,6 +60,6 @@ public class MovieClient {
 		});
 
 		String uriString = webTarget.getUri().toString();
-		System.out.println("\n"+uriString + ":: is being processed synchronously::\n" + movies);
+		System.out.println("\n" + uriString + ":: is being processed synchronously::\n" + movies);
 	}
 }
