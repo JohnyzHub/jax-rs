3249851a36b694155e85357f7b2ebf5aa8cd6140
johnyzhub <johnyzhub@gmail.com>
2019-09-15 11:11:28 -0400

    JAX-RS: Server side filters
       Added server side filters

---
+++ moviedirectory/src/main/java/com/javaee/ws/restful/service/filter/request/MovieServiceRequestFilter.java
@@ -0,0 +1,40 @@
+package com.javaee.ws.restful.service.filter.request;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * @author johnybasha
+ *
+ */
+@Provider
+@PreMatching
+public class MovieServiceRequestFilter implements ContainerRequestFilter {
+
+	private String lOCATIONString = "BERMUDA";
+
+	public MovieServiceRequestFilter() {
+	}
+
+	/**
+	 * This method will be triggered after the jax-rs container identifying the
+	 * matching resource for the request by deault. If the class is annotated
+	 * with @PreMatching, the filter will be triggered by the jax-rs container
+	 * before identifying the matching resource.
+	 */
+	@Override
+	public void filter(ContainerRequestContext requestContext) throws IOException {
+		System.out.println("<<<<!!!Inside server side request filter!!!>>>>");
+		String locationString = requestContext.getHeaderString(HttpHeaders.LOCATION);
+		if (locationString != null && locationString.equalsIgnoreCase(lOCATIONString)) {
+			requestContext.abortWith(Response.status(Status.UNAUTHORIZED).entity("Access Unauthorized").build());
+		}
+	}
+}
---
+++ moviedirectory/src/main/java/com/javaee/ws/restful/service/filter/response/MovieServiceResponseFilter.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.javaee.ws.restful.service.filter.response;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * @author johnybasha
+ *
+ */
+@Provider
+public class MovieServiceResponseFilter implements ContainerResponseFilter {
+
+	private String lOCATIONString = "BERMUDA";
+
+	public MovieServiceResponseFilter() {
+	}
+
+	@Override
+	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+			throws IOException {
+
+		String locationString = requestContext.getHeaderString(HttpHeaders.LOCATION);
+		if (locationString == null || !locationString.equalsIgnoreCase(lOCATIONString)) {
+			responseContext.getHeaders().add(HttpHeaders.CONTENT_LANGUAGE, "ENGLISH");
+		}
+	}
+
+}
